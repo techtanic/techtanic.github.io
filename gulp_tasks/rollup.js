05a12f5fb8e88b0556b9d9291ee51d0d80470d03
TECHTANIC <64263079+techtanic@users.noreply.github.com>
2021-03-16 10:12:01 +0300

    .

--- gulp_tasks/rollup.js
+++ gulp_tasks/rollup.js
@@ -9,22 +9,20 @@ var gulp = require('gulp'),
 
 // Bundle ES6 modules.
 gulp.task('rollup:main', function() {
-  return gulp.src(paths.dev.js + '/**/*.js')
-    .pipe(plumber())
-    .pipe(rollup({
-      format: 'umd',
-      entry: [
-        paths.dev.js + '/body.js',
-        paths.dev.js + '/head.js'
-      ],
-      plugins: [
-        babel({
-          exclude: 'node_modules/**',
-          presets: ['@babel/preset-env'],
-          babelrc: false
-        })
-      ]
-    }))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(paths.build.js));
+    return gulp.src(paths.dev.js + '/**/*.js')
+        .pipe(plumber())
+        .pipe(rollup({
+            entry: [
+                paths.dev.js + '/body.js',
+                paths.dev.js + '/head.js'
+            ],
+            plugins: [
+                babel({
+                    exclude: 'node_modules/**',
+                    presets: ['es2015-rollup']
+                })
+            ]
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.build.js));
 });
